a0011550108d55151b7c37d1ba0690215d2d7686
SMARTRON7 <smartabbas07@gmail.com>
2024-08-21 22:57:30 +0530

    Adjusted mobile header

--- My_Portfolio/src/components/Header/Header.jsx
+++ My_Portfolio/src/components/Header/Header.jsx
@@ -108,7 +108,7 @@ function Header() {
                                     </button></Scroll>
                             </li>
 
-                            {scrollItems.map((item) =>
+                            {!hideAboutAndWork && scrollItems.map((item) =>
                                 <li key={item.name} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full cursor-pointer'>
                                     {console.log(item.name)}
                                     <Scroll
